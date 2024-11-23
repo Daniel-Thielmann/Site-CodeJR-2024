dea9bee383ae3bd1ed99190727554a3915c25cc3
Daniel Thielmann <thielmanndan@gmail.com>
2024-11-23 00:16:55 -0300

    fixing

--- src/app/individualBlog/[id]/page.tsx
+++ src/app/individualBlog/[id]/page.tsx
@@ -2,18 +2,18 @@ import BlogIndividual from "@/components/BlogIndividual";
 import { RichText } from "@/components/RichText/rich-text";
 import { getBlog1 } from "@/server/db/query/blog/query";
 
-export default async function Page({ params }: { params: { id: string } }){
-    const blog1 = await getBlog1(params.id);
-    console.log(blog1);
-    
-    return(
-        <div className="bg-gradiente-portifolio min-h-screen">
-            <BlogIndividual 
-                title={blog1.title}
-                text={<RichText content={blog1.text.raw} />}
-                date={blog1.date}
-                image={blog1.image.url} 
-            />
-        </div>
-    )
+export default async function Page({ params }: { params: { id: string } }) {
+  const blog1 = await getBlog1(params.id);
+  console.log(blog1);
+
+  return (
+    <div className="bg-gradiente-portifolio min-h-screen">
+      <BlogIndividual
+        title={blog1.title}
+        text={<RichText content={blog1.text.raw} />}
+        date={blog1.date}
+        image={blog1.image.url}
+      />
+    </div>
+  );
 }
